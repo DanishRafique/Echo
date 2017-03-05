663b3ff17ee2ded0d78f720edc26ce8adcafab41
subhamayd2 <bloodyshower@gmail.com>
2017-03-05 20:00:47 +0530

    Sign Up

---
+++ app/src/main/java/in/co/echoindia/echo/User/SignupActivity.java
@@ -0,0 +1,15 @@
+package in.co.echoindia.echo.User;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.co.echoindia.echo.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
