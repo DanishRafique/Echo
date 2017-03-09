2e912eb3b54117a3e5a6f1eb372916cb22a3e737
Danish Rafique <danishrafique0154@gmail.com>
2017-03-10 00:42:37 +0530

    Login

---
+++ app/src/main/java/in/co/echoindia/echo/Model/UserDetailsModel.java
@@ -0,0 +1,164 @@
+package in.co.echoindia.echo.Model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Danish Rafique on 09-03-2017.
+ */
+
+public class UserDetailsModel implements Serializable{
+    private String userName="";
+    private String password="";
+    private String firstName="";
+    private String lastName="";
+    private String emailId="";
+    private String phoneNo="";
+    private String address="";
+    private String ward="";
+    private String city="";
+    private String pinCode="";
+    private String district="";
+    private String state="";
+    private String userPhoto="";
+    private String aadhaarPhoto="";
+    private String voterIdPhoto="";
+    private boolean issueMaker;
+    private boolean isVerified;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWard() {
+        return ward;
+    }
+
+    public void setWard(String ward) {
+        this.ward = ward;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+
+    public String getAadhaarPhoto() {
+        return aadhaarPhoto;
+    }
+
+    public void setAadhaarPhoto(String aadhaarPhoto) {
+        this.aadhaarPhoto = aadhaarPhoto;
+    }
+
+    public String getVoterIdPhoto() {
+        return voterIdPhoto;
+    }
+
+    public void setVoterIdPhoto(String voterIdPhoto) {
+        this.voterIdPhoto = voterIdPhoto;
+    }
+
+    public boolean issueMaker() {
+        return issueMaker;
+    }
+
+    public void setIssueMaker(boolean issueMaker) {
+        this.issueMaker = issueMaker;
+    }
+
+    public boolean isVerified() {
+        return isVerified;
+    }
+
+    public void setVerified(boolean verified) {
+        isVerified = verified;
+    }
+
+}
--- app/src/main/java/in/co/echoindia/echo/Utils/Constants.java
+++ app/src/main/java/in/co/echoindia/echo/Utils/Constants.java
@@ -7,4 +7,8 @@ package in.co.echoindia.echo.Utils;
 public class Constants {
     public static final String SETTINGS_APP_SETTINGS = "ECHO USER";
     public static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+    public static final String SETTINGS_IS_LOGGED = "IS_LOGIN";
+    public static final String SETTINGS_IS_LOGGED_TYPE = "LOGIN_TYPE";
+    public static final String SETTINGS_IS_LOGGED_USER_CODE = "USER_CODE";
+    public static final String SETTINGS_OBJ_USER= "USER_OBJECT";
 }
